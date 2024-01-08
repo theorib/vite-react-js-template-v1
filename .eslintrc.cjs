eefb195e9bb8f9b4206df4b10fb5d3fdfe301239
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-01-08 21:31:53 +0000

    Add more robust ESlint rules and simplify App.jsx

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -8,13 +11,21 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: { version: '18.2' },
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/prop-types': 'off', // Disable the prop-types rule
+    // 'no-unused-vars': 'off', // Disable the unused variable warnings
+    // ...you can add more custom rules here
   },
-}
+};
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,3 @@
-import '../index.css';
-
 export default function App() {
   return <>Hellow React</>;
 }
